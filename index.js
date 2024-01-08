436f70b9b866e2ac3a70c2f8785e70a23feb09a0
Dibyajyoti06 <dibyajyotinaik79@gmail.com>
2024-01-08 22:30:26 +0530

    change js file

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ let string = '';
 let arr = Array.from(buttons);
 arr.forEach((buttons) => {
   buttons.addEventListener('click', (e) => {
-    if (e.target.textContent == '=') {
+    if (e.target.innerHTML == '=') {
       string = eval(string);
       input.value = string;
-    } else if (e.target.textContent == 'AC') {
+    } else if (e.target.innerHTML == 'AC') {
       input.value = '';
       string = '';
-    } else if (e.target.textContent == 'DEL') {
+    } else if (e.target.innerHTML == 'DEL') {
       newString = string.slice(0, -1);
       string = newString;
       input.value = string;
     } else {
-      string += e.target.textContent;
+      string += e.target.innerHTML;
       input.value = string;
     }
   });
