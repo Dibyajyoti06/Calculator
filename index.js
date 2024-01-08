ad284b35ec6562d3908c8323ea5d97d8adada030
Dibyajyoti06 <dibyajyotinaik79@gmail.com>
2024-01-08 22:23:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const input = document.getElementById('input-Box');
+let buttons = document.querySelectorAll('button');
+let string = '';
+let arr = Array.from(buttons);
+arr.forEach((buttons) => {
+  buttons.addEventListener('click', (e) => {
+    if (e.target.textContent == '=') {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.textContent == 'AC') {
+      input.value = '';
+      string = '';
+    } else if (e.target.textContent == 'DEL') {
+      newString = string.slice(0, -1);
+      string = newString;
+      input.value = string;
+    } else {
+      string += e.target.textContent;
+      input.value = string;
+    }
+  });
+});
