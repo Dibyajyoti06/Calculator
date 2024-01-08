404ff1c1dd46824d2fc75a593818f3400b4a22e5
Dibyajyoti06 <dibyajyotinaik79@gmail.com>
2024-01-08 22:54:21 +0530

    changing js file

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ arr.forEach((buttons) => {
     if (e.target.innerHTML == '=') {
       string = eval(string);
       input.value = string;
-    } else if (e.target.innerHTML == 'AC') {
+    } else if (e.target.textContent == 'AC') {
       input.value = '';
       string = '';
-    } else if (e.target.innerHTML == 'DEL') {
+    } else if (e.target.textContent == 'DEL') {
       newString = string.slice(0, -1);
       string = newString;
       input.value = string;
     } else {
-      string += e.target.innerHTML;
+      string += e.target.textContent;
       input.value = string;
     }
   });
